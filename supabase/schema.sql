1813a32a4a8993f5786051f7f6d75fe93e1335fe
mm4you <mm4you@users.noreply.github.com>
2026-08-29 19:19:01 +0700

    fix(supabase): add schema grants for anon and authenticated roles

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -127,3 +127,14 @@ BEGIN
     WHERE id = t_id;
 END;
 $$;
+
+-- ==============================================================================
+-- GRANT PERMISSIONS TO ANON & AUTHENTICATED ROLES
+-- ==============================================================================
+GRANT USAGE ON SCHEMA public TO anon, authenticated, service_role;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO anon, authenticated, service_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO anon, authenticated, service_role;
+GRANT ALL ON ALL ROUTINES IN SCHEMA public TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
